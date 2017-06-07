ca8cc7e316a8f9498f128d017e2ac88546c4b2e7
Julyettka <julyettka@gmail.com>
2017-06-07 17:54:07 +0300

    title change js, new css

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,12 @@
-$(function(){
-//   $("body").fadeOut(); 
-});
+
+function getText(id){
+    var item = document.getElementById(id).innerHTML;
+    console.log(item);
+    var title = document.getElementById("titleHistoryNav");
+    title.innerHTML = "";
+    title.innerHTML = item;
+}
+
+
+
+
