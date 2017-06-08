e03e54caef49f78f8c437832d1c601f59326e618
Julyettka <julyettka@gmail.com>
2017-06-08 14:03:51 +0300

    burger add

--- app/js/common.js
+++ app/js/common.js
@@ -1,11 +1,23 @@
-
 function getText(id){
+   
     var item = document.getElementById(id).innerHTML;
+    console.log(id);
     console.log(item);
     var title = document.getElementById("titleHistoryNav");
     title.innerHTML = "";
     title.innerHTML = item;
+    
+    function changeText(id){
+//        switch(id){
+//            case item1:
+//                
+//                
+//        }
+                
+    }
 }
+
+
 
 
 
